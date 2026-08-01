2b210ffc5c9f11cf671005170780e8c5dab649c7
Thomas Gauthier <thomas.gauthier.1@tuta.com>
2026-08-01 14:41:33 -0400

    fix(transmission): removed impl Payload for Box<dyn Payload> (unused + UB with RC)
    Signed-off-by: Thomas Gauthier <thomas.gauthier.1@tuta.com>

--- src/server/transmission.rs
+++ src/server/transmission.rs
@@ -1,65 +1,13 @@
-use std::rc::Rc;
 use std::string::ToString;
 
 use super::jobs::{FulfilledJob, JobDesc, Pid};
 
-trait PayloadIntern {
-    fn from_str_box(self: Rc<Self>, data: &str)
-    -> Result<Box<dyn Payload + 'static>, PayloadError>;
-}
-
-// Since we only implement Payload for concrete types and Box<dyn Paylaod>
-// we justify this use of 'static. In addition, the method 'from_str_static'
-// implies a concrete type. Anything else should fail.
-impl<T> PayloadIntern for T
-where
-    T: Payload + 'static,
-{
-    fn from_str_box(
-        self: Rc<Self>,
-        data: &str,
-    ) -> Result<Box<dyn Payload + 'static>, PayloadError> {
-        <Self as Payload>::from_str_static(data)
-            .map(|x| -> Box<dyn Payload + 'static> { Box::new(x) })
-    }
-}
-
-pub trait Payload: PayloadIntern {
+pub trait Payload {
     fn from_str_static(data: &str) -> Result<Self, PayloadError>
     where
         Self: Sized;
 
-    fn from_str(&self, data: &str) -> Result<Self, PayloadError>
-    where
-        Self: Sized,
-    {
-        <Self as Payload>::from_str_static(data)
-    }
-
     fn to_string(&self) -> String;
-}
-
-impl Payload for Box<dyn Payload> {
-    fn from_str_static(_: &str) -> Result<Self, PayloadError>
-    where
-        Self: Sized,
-    {
-        unreachable!("Someone thought it was a good idea to parse something concrete with dyn.");
-    }
-
-    fn from_str(&self, data: &str) -> Result<Self, PayloadError>
-    where
-        Self: Sized,
-    {
-        <dyn Payload as PayloadIntern>::from_str_box(
-            unsafe { Rc::from_raw(&**self as *const dyn Payload) },
-            data,
-        )
-    }
-
-    fn to_string(&self) -> String {
-        (**self).to_string()
-    }
 }
 
 pub struct PayloadError {
