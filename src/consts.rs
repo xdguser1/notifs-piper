ad3b806cc271b1647695f068a819742e44bafba8
Thomas Gauthier <thomas.gauthier.1@tuta.com>
2026-08-26 14:22:41 -0400

    fix(all): version
    Signed-off-by: Thomas Gauthier <thomas.gauthier.1@tuta.com>

--- src/consts.rs
+++ src/consts.rs
@@ -1,5 +1,5 @@
 /***** DBUS CONSTANTS *****/
-pub const VERSION: &str = "0.1.0";
+pub const VERSION: &str = "0.1.1";
 pub const NAME: &str = "notifs-piper";
 pub const NOTIFICATIONS_PROT_VER: &str = "1.3";
 
